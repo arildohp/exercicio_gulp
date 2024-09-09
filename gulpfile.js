9a94177fe1dfb80c9de2f535d66162dbf1645dbc
arildo <arildohp@gmail.com>
2024-09-09 17:38:40 -0300

    iniciando exercicio

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+function compilaSass() {
+    return gulp.src('./source/styles/main.scss')
+      .pipe(sass())
+      .pipe(gulp.dest('./build/styles'));
+}
+
+
+
+exports.default = gulp;
+exports.sass = compilaSass;
