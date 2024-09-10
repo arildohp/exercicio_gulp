aeda6c0b4642f4f949fc9e3a47c5c08ec320cd75
arildo <arildohp@gmail.com>
2024-09-10 08:00:15 -0300

    minificando js com gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const sourcemaps = require('gulp-sourcemaps')
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+
+function comprimeJavascript() {
+   return gulp.src('./source/scripts/*.js')
+      .pipe(uglify())
+      .pipe(gulp.dest('./build/scripts'))
+}
 
 function compilaSass() {
     return gulp.src('./source/styles/main.scss')
@@ -19,3 +26,4 @@ exports.sass = compilaSass;
 exports.watch = function() {
    gulp.watch('./source/styles/*.scss', { ignoreInitial: false }, gulp.series(compilaSass));
 }
+exports.javascript = comprimeJavascript;
